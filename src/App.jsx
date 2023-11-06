97d47e988170db97d737bea9025831521bc6302c
PRITAM CHAKROBORTY <92992609+anynomous001@users.noreply.github.com>
2023-11-06 13:54:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,155 @@
+
+import React from 'react';
+import './App.css';
+import Questions from './Questions';
+import { nanoid } from 'nanoid';
+import { shuffleArray, LoadingSpinner } from './utils'
+import { options } from './topicoptions'
+import { QuizReducer, Initial_State } from './QuizReducer';
+
+
+
+
+
+
+
+function App() {
+
+  const [questions, setQuestions] = React.useState([]);
+  const [error, setError] = React.useState(null)
+  const [start, setStart] = React.useState(false)
+  const [state, dispatch] = React.useReducer(QuizReducer, Initial_State)
+
+
+
+  async function fetchQuestions(category, level) {
+    try {
+      const res = await fetch(`https://opentdb.com/api.php?amount=5&category=${category}&difficulty=${level}&type=multiple`);
+      if (!res.ok) {
+        throw new Error('Error took place' + res.status);
+      }
+      const data = await res.json();
+
+      return data.results;
+    } catch (error) {
+      throw error; // Re-throw the error
+    }
+  }
+
+
+
+
+  const getQuestions = async (e) => {
+    dispatch({ type: 'Fetching_started' })
+    setStart(true)
+    e.preventDefault()
+
+    console.log(e.target.category.value)
+
+    const category = e.target.category.value
+    const level = e.target.level.value
+
+    setTimeout(async () => {
+      try {
+        const questions = await fetchQuestions(category, level);
+        setError(null); // Clear any previous errors
+        setQuestions(() => {
+          return questions.map((question) => {
+            const correctAnswer = {
+              id: nanoid(),
+              value: question.correct_answer,
+            }
+
+            const incorrectAnswers = question.incorrect_answers.map(value => ({
+              id: nanoid(),
+              value,
+            }))
+
+            // [
+            //   { id: 'fsadfq34aegvds', value: 'Apple' },
+            //   { id: '4g3qefgvewdcew', value: 'Pear' },
+            // ]
+
+            return {
+              question: question.question,
+              id: nanoid(),
+              answers: shuffleArray([...incorrectAnswers, correctAnswer]),
+              correctAnswerId: correctAnswer.id,
+              selectedAnswerId: null,
+            };
+          });
+        });
+      } catch (error) {
+        setError(error.message); // Set the error message
+      }
+      dispatch({ type: 'Fetching-success' }); // Mark loading as completed
+    }, 2000);
+  };
+
+  const selectAnswer = (selectedQuestionId, selectedAnswerId) => {
+    console.log(state + 'appp')
+    setQuestions(prevData => {
+      return prevData.map(question => {
+        if (question.id !== selectedQuestionId) return question
+        return {
+          ...question,
+          selectedAnswerId,
+        }
+      })
+    })
+  }
+
+
+  return (
+    <div className="app">
+      <div className="main">
+        <div className={`start-container ${start ? 'game-start' : ''}`}>
+          <h1>Quizzicle</h1>
+          <form id='quiz-form' onSubmit={(e) => getQuestions(e)}>
+            <p className='label-category' >Select Category :</p>
+            <select id='category' className='category' name="category">
+              {options.map((option) => {
+                return (
+                  <option key={option.value} value={option.value}>{option.label}</option>
+                )
+              })}
+            </select>
+
+
+            <p className='label-difficulty' >Select Difficulty :</p>
+            <select id="level" className='level' name="level">
+              <option value='easy'>Easy</option>
+              <option value='medium'>Medium</option>
+              <option value='hard'>Hard</option>
+            </select>
+            <button className="start-btn" type='submit' >
+              Start Quiz
+            </button>
+          </form>
+        </div>
+
+        <div className="quiz-setup">
+          {state.loading ? (
+            <LoadingSpinner />
+          ) : (
+            <Questions
+              error={error}
+              questions={questions}
+              start={{ start, setStart }}
+
+
+              loading={state.loading}
+              selectAnswer={selectAnswer}
+            />
+          )}
+
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Questions.jsx
@@ -0,0 +1,250 @@
+import React from 'react'
+import './App.css'
+import { nanoid } from 'nanoid';
+import { QuizReducer, Initial_State } from './QuizReducer';
+
+// How to determine which one to use: useState OR useReducer
+// useReducer ---> If you have states which are "coupled together"/"depend on each other"
+// useState --->  When you states are not depending on each other, can live separately (in any other cases)
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+// Derive from your state whenever you can, do not create a new state just to save a new value
+// console.log(questions)
+
+// let correctCount = 0;
+// for (let i = 0; i < questions.length; i++) {
+//     const question = questions[i]
+
+//     if (false) {
+//         correctCount++
+//     }
+// }
+
+// const correctCount2 = questions.reduce((acc, question) => {
+//     // some branching
+// }, 0)
+
+// const correctCountMemo = React.useMemo(() => {
+//     if (!isGameOn) return 0
+
+//     let count = 0;
+
+//     for (let i = 0; i < questions.length; i++) {
+//         const question = questions[i]
+
+//         if (false) {
+//             count++
+//         }
+//     }
+
+//     return count
+// }, [questions])
+
+
+// const correctCount2Memo = React.useMemo(() => {
+//     if (!isGameOn) return 0
+
+//     return questions.reduce((acc, question) => {
+//         // some branching
+//     }, 0)
+// }, [questions])
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+// You can be creative by extracting logic into your own custom hooks
+// const requestStatuses = {
+//     Idle: 'idle',
+//     Pending: 'pending',
+//     Resolved: 'resolves',
+//     Rejected: 'rejected',
+// }
+
+// const useStatus = (defaultStatus = requestStatuses.Idle) => {
+//     const [status, setStatus] = React.useState(defaultStatus)
+
+//     const isIdle = status === requestStatuses.Idle
+//     const isPending = status === requestStatuses.Pending
+//     const isResolved = status === requestStatuses.Resolved
+//     const isRejected = status === requestStatuses.Rejected
+
+//     return {
+//         isIdle,
+//         isPending,
+//         isResolved,
+//         isRejected,
+//         status,
+//         setStatus,
+//     }
+// }
+
+
+// const decreaseCounter = () => {
+//     dispatch({ type: 'decrease' })
+// }
+
+// const { isIdle, isPending, isResolved, isRejected, } = useStatus()
+
+// const hasError = error != null // ---> error !== undefined && error !== null
+
+// if (isIdle) {
+//     return ???
+// }
+
+// if (isPending && hasError) {
+//     return 'We cought and erro but still trying to load the data'
+// }
+
+// if (isPending && !hasError) {
+
+// }
+
+
+// What to do next:
+// 1. Use "selectAnswer" in Questions component
+// 2. Derive correctCount from your state
+// 3. Have a think how you could derive the styling based on your state values:
+//    (selectedAnswerId, correctAnswerId and start)
+// 4. Add prettier to your codebase and setup your IDE to format on save (latter is optional but recommended)
+// Don't forget to add a script to you package.json that formats your files inside your "./src" directory
+
+
+/*ShuffleArray function for Shuffling correct and incorrect answers
+    
+answers: (4) ['Notebook', 'Money', 'Watch', 'Keys']
+correctAnswer: "Watch"
+id: "SX9AnH7pckZXpCr2zeJiy"
+question: "In past times, what would a gentleman keep in his fob pocket?"
+ 
+*/
+
+
+
+/* setSelectedAnswers((prevAnswers) => {
+     if (prevAnswers)
+         return {
+             ...prevAnswers,
+             [questionIndex]: userSelectedAnsId,
+         }
+     else {
+         return {
+             [questionIndex]: userSelectedAnsId,
+         }
+     }
+ 
+ })*/
+
+// setSelectedAnswers((prevAnswers) => {
+//     let updatedAnswers = []
+//     if (prevAnswers) {
+//         updatedAnswers = [...prevAnswers];
+//         updatedAnswers[questionIndex] = userSelectedAnsId;
+//         return updatedAnswers;
+
+//     } else {
+
+//         updatedAnswers[questionIndex] = userSelectedAnsId;
+//         return updatedAnswers;
+
+//     }
+// });
+// console.log(selectedAnswers)
+
+
+const Questions = ({ error, start, loading, questions, selectAnswer }) => {
+
+    const [state, dispatch] = React.useReducer(QuizReducer, Initial_State)
+    const [correctAnswers, setCorrectAnswers] = React.useState({});
+    const [wrongAnswers, setWrongAnswers] = React.useState({});
+
+    const quizQuestions = questions?.map((question, questionIndex) => {
+
+        const isCorrect = correctAnswers[question.id];
+        const isWrong = wrongAnswers[question.id];
+
+
+        return (
+            <div className='quizes' key={questionIndex}>
+                <h3>{question.question}</h3>
+                <div className='answers-div'>
+                    {question.answers?.map((answer, index) => {
+
+                        const isAnswerSelected = answer.id === question.selectedAnswerId;
+                        const correctAnswer = answer.id === question.correctAnswerId
+
+                        return <button
+                            key={answer.id}
+                            id={answer.id}
+                            onClick={() => selectAnswer(question.id, answer.id)}
+                            disabled={state.answerChecked && !correctAnswer && !isAnswerSelected && true}
+                            className={`
+                            answer_span ${isAnswerSelected ? 'select' : ''}
+                             ${correctAnswer && state.answerChecked ? 'correct' : ''
+                                } ${isWrong && isAnswerSelected ? 'wrong' : ''}
+                                `}
+                        >{answer.value}</button>
+                    })}
+                </div>
+                <hr></hr>
+            </div>
+        )
+    })
+
+
+
+
+    function checkAnswers() {
+        dispatch({ type: 'Answer_checked' })
+
+
+        const correctAnswersObj = {};
+        const wrongAnswersObj = {};
+
+        questions.forEach((question) => {
+            if (question.selectedAnswerId === question.correctAnswerId) {
+                dispatch({ type: 'correct-answer' })
+
+                correctAnswersObj[question.id] = true;
+            } else {
+                wrongAnswersObj[question.id] = true;
+            }
+        });
+
+        setCorrectAnswers(correctAnswersObj);
+        setWrongAnswers(wrongAnswersObj);
+    }
+
+
+    return (
+
+        start.start &&
+
+        <div className='quiz-div'>
+            {loading && <p>Loading....</p>}
+            {error && <h1>{error}</h1>}
+            {!error && quizQuestions}
+
+            {!state.answerChecked && (
+                <button className="check-btn" onClick={checkAnswers}>Check Answer</button>
+            )}
+
+            {state.answerChecked &&
+                <>
+                    <button className={`play-again-btn`}
+                        onClick={() => {
+                            start.setStart(false);
+                            dispatch({ type: 'game-restarted' })
+                        }} key={nanoid()} >Play Again</button>
+
+                    <h3 className='score-msg'>You got <span className='score'>{state.count}/{questions.length}</span> Correct Answers</h3>
+                </>
+
+            }
+        </div >
+
+    )
+}
+export default Questions;
+
